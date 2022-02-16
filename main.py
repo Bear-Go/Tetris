48204734b300a924ff08f14391bcafca14a8ae85
Bear-Go <jerry2262@outlook.com>
2022-02-17 01:19:03 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+# from window import TetrisWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    # ex = TetrisWindow()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
